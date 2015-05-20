ee7684377c5b5ba008da1c97dcfca3a19e8e5fd6
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-05-20 14:14:59 +0300

    Extracted user_signup helper method

---
+++ spec/login_helper.rb
@@ -0,0 +1,8 @@
+def user_signup email='sample@something.ie', password='moomoocow'
+  visit '/'
+  click_link 'Sign up'
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
+  click_button 'Sign up'
+end

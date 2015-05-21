ff7bd636fed5986dd3022c365ead2ef11e22b42e
Mark Pokorny <iridium193@gmail.com>
2015-05-21 01:57:09 +0100

    Added ‘js: true’ to test JQuery manipulation of page elements. Removed unneeded delay helper.

--- spec/features/invite_contributors_spec.rb
+++ spec/features/invite_contributors_spec.rb
@@ -1,5 +1,3 @@
-require 'delay_helper'
-
 feature 'Invite Contributors' do
   context 'when logged in and with a prezzy being made' do
     before do
@@ -9,11 +7,10 @@ feature 'Invite Contributors' do
       prepare_prezzy
     end
 
-    scenario 'user can invite contributors', :js => true do
+    scenario 'user can invite contributors', js: true do
       visit '/gifts/new'
       click_button 'Add contributors'
-      wait_for_ajax
-      expect(page).to have_css('#contributor_1[name="contributor_email"]')
+      expect(page).to have_css 'input[id="contributor_1"]'
     end
   end
 end

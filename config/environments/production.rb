e3f3ca41c2648126ed6e27585f3c9ad791a72186
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-21 23:14:17 +0100

    Logger level update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,3 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ActiveRecord::Base.logger.level = Logger::DEBUG

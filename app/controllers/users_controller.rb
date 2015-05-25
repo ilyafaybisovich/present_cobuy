744e323dcc07aaeaf7ac7e05af1c250e9d6a6618
Mark Pokorny <iridium193@gmail.com>
2015-05-25 23:49:39 +0100

    User profile tests overhauled.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,11 @@ class UsersController < ApplicationController
 
   def show
     return unless user_signed_in?
-    @user = User.find params[:id]
-    @gifts = Gift.where user_id: @user.id
+    @user = User.find current_user.id
+    contributors = Contributor.where email: @user.email
+    @gifts = []
+    contributors.each do |contributor|
+      @gifts << Gift.find(contributor.gift_id)
+    end
   end
 end

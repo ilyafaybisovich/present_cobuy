ddf60928f76da737c21018bc2c5840b730dc94b5
Mark Pokorny <iridium193@gmail.com>
2015-05-26 01:25:49 +0100

    ContributorMailer unit test suite overhauled.

--- spec/mailers/contributor_mailer_spec.rb
+++ spec/mailers/contributor_mailer_spec.rb
@@ -1,22 +1,20 @@
 RSpec.describe ContributorMailer, type: :mailer do
+  let(:item) { double(:item, title: 'A Birthday Gift') }
+  let(:gift) do
+    double(:gift, item: item, title: 'Dad’s Birthday',
+                  organiser: 'Rob', description: '')
+  end
+  let(:contributor) { double(:contributor, email: 'rob@rbgeomatics.co.uk') }
+  let(:mail) { described_class.invited(gift, contributor) }
 
-  describe "invited" do
+  it 'renders the headers' do
 
-    let(:item) { double(:item, title: "A Birthday Gift") }
-    let(:gift) { double(:gift, item: item, title: "Dads Birthday", organiser: "Rob", description: "") }
-    let(:contributor) { double(:contributor, email: "rob@rbgeomatics.co.uk") }
-    let(:mail) { described_class.invited(gift, contributor) }
-
-    xit "renders the headers" do
-
-      expect(mail.subject).to match("You can make this happen!")
-      expect(mail.to).to eq(["rob@rbgeomatics.co.uk"])
-      expect(mail.from).to eq(["makers_academy_test@rbgeomatics.co.uk"])
-    end
-
-    xit "renders the body" do
-      expect(mail.body.encoded).to match("Hello there")
-    end
+    expect(mail.subject).to match 'You can make this happen!'
+    expect(mail.to).to eq ['rob@rbgeomatics.co.uk']
+    expect(mail.from).to eq ['makers_academy_test@rbgeomatics.co.uk']
   end
 
+  it 'renders the body' do
+    expect(mail.body.encoded).to match 'Hello there'
+  end
 end

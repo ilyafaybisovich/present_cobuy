32aa8f153263592b0e0e5f38f98cd8d5f2bbca0c
Mark Pokorny <iridium193@gmail.com>
2015-05-27 16:23:28 +0100

    Removed uniqueness from user names.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527120249) do
+ActiveRecord::Schema.define(version: 20150527152115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,12 +65,11 @@ ActiveRecord::Schema.define(version: 20150527120249) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "contributors", "gifts"
   add_foreign_key "gifts", "users"

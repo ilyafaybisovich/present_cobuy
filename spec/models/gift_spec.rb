4d1bdcd16c3a35853a3fbe837ea9634600656701
Mark Pokorny <iridium193@gmail.com>
2015-05-29 10:25:13 +0100

    Really minor adjustments.

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Gift, type: :model do
   it { is_expected.to have_many :contributors }
 
-  it 'it can calculate a split price' do
+  it 'calculates a split price' do
     gift = described_class.create(item_price: 200.0)
     gift.contributors.create(email: 'test@test.com')
     gift.contributors.create(email: 'test2@test.com')
@@ -9,7 +9,7 @@ RSpec.describe Gift, type: :model do
     expect(gift.split_price).to eq '100.0'
   end
 
-  it 'it re-calculates a split price when a contributor is removed' do
+  it 're-calculates a split price when a contributor is removed' do
     gift = described_class.create(item_price: 200.0)
     gift.contributors.create(email: 'test@test.com')
     gift.contributors.create(email: 'test2@test.com')
@@ -34,7 +34,7 @@ RSpec.describe Gift, type: :model do
     expect(gift.percentage_complete).to eq 50
   end
 
-  it 'can respond with whether the contribution is complete' do
+  it 'detects that not everyone has contributed' do
     gift = described_class.create(item_price: 200.0)
     gift.contributors.create(email: 'test@test.com', token: "ffndjfnrjfgnw")
     gift.contributors.create(email: 'test2@test.com')
@@ -42,7 +42,7 @@ RSpec.describe Gift, type: :model do
     expect(gift.all_contributed?).to be_falsy
   end
 
-  it 'can respond with whether the contribution is complete' do
+  it 'detects when everyone has contributed' do
     gift = described_class.create(item_price: 200.0)
     gift.contributors.create(email: 'test@test.com', token: "ffndjfnrjfgnw")
     gift.contributors.create(email: 'test2@test.com', token: "ffndjfnrjfgnw")
